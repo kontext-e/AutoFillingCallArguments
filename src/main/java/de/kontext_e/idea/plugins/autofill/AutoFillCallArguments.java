70464507e93157a3850bde7ffa6013951dff71e9
WilliamThimm <William.Thimm@gmail.com>
2024-07-23 11:54:26 +0200

    Minor code cleanup

--- src/main/java/de/kontext_e/idea/plugins/autofill/AutoFillCallArguments.java
+++ src/main/java/de/kontext_e/idea/plugins/autofill/AutoFillCallArguments.java
@@ -10,6 +10,7 @@ import com.intellij.lang.parameterInfo.ParameterInfoHandler;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.command.CommandProcessor;
 import com.intellij.openapi.components.Service;
+import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.DumbService;
 import com.intellij.openapi.project.Project;
@@ -42,71 +43,121 @@ import java.util.stream.Collectors;
 
 import static com.intellij.openapi.command.UndoConfirmationPolicy.DEFAULT;
 
-@Service(Service.Level.PROJECT)
+@Service
 public final class AutoFillCallArguments extends PsiElementBaseIntentionAction implements IntentionAction {
+
     @Override
     public void invoke(@NotNull final Project project, final Editor editor, @NotNull final PsiElement psiElement) throws IncorrectOperationException {
-        if (editor == null) {
-            return;
-        }
+        if (editor == null) {return; }
+        if (!ApplicationManager.getApplication().isDispatchThread()) { return; }
 
-        if (!ApplicationManager.getApplication().isDispatchThread()) {
-            return;
-        }
         PsiDocumentManager.getInstance(project).commitAllDocuments();
 
         final var psiMethods = resolveMethodFromCandidates(project, editor);
-        if (psiMethods.isEmpty()) {
-            return;
-        }
+        if (psiMethods.isEmpty()) { return; }
+
         if (psiMethods.size() == 1) {
             insertParameters(editor, psiMethods.iterator().next());
         } else {
-            insertParameters(project, editor, psiMethods);
+            insertParametersForOverloadedMethod(project, editor, psiMethods);
         }
     }
 
-    private void insertParameters(final Project project, final Editor editor, final Collection<PsiMethod> psiMethods) {
-        // Create a popup dialog that displays the list of options
-        final var methodsWrapped = psiMethods.stream().map(PsiMethodWrapper::new).toList();
+    private Collection<PsiMethod> resolveMethodFromCandidates(@NotNull final Project project, final Editor editor) {
+        final PsiFile file = PsiUtilBase.getPsiFileInEditor(editor, project);
+        final int offset = editor.getCaretModel().getOffset();
+        final int fileLength = file.getTextLength();
 
+        final ShowParameterInfoContext context = new ShowParameterInfoContext(editor, project, file, offset, -1, false, false);
+
+        final int offsetForLangDetection = offset > 0 && offset == fileLength ? offset - 1 : offset;
+        final Language language = PsiUtilCore.getLanguageAtOffset(file, offsetForLangDetection);
+
+        return DumbService.getInstance(project).computeWithAlternativeResolveEnabled(
+                () -> getParameterInfoHandlers(project, file, language)
+                        .stream()
+                        .map(handler -> handler.findElementForParameterInfo(context))
+                        .filter(Objects::nonNull)
+                        .flatMap(element -> Arrays.stream(context.getItemsToShow()))
+                        .map(MethodParameterInfoHandler::tryGetMethodFromCandidate)
+                        .filter(Objects::nonNull)
+                        .filter(PsiMethod::hasParameters)
+                        .toList()
+        );
+    }
+
+    @NotNull
+    private static Collection<ParameterInfoHandler> getParameterInfoHandlers(@NotNull final Project project, @NotNull final PsiFile file, @NotNull final Language language) {
+        final var handlers = new HashSet<ParameterInfoHandler>();
+        final var dumbService = DumbService.getInstance(project);
+
+        for (final var currentLanguage : List.of(language, file.getViewProvider().getBaseLanguage())) {
+            handlers.addAll(dumbService.filterByDumbAwareness(LanguageParameterInfo.INSTANCE.allForLanguage(currentLanguage)));
+        }
+
+        return handlers;
+    }
+
+    private void insertParametersForOverloadedMethod(final Project project, final Editor editor, final Collection<PsiMethod> psiMethods) {
+        // Wrap methods to customize toString() behavior
+        final var methodsWrapped = psiMethods.stream().map(PsiMethodWrapper::new).toList();
         final var model = new DefaultListModel<PsiMethodWrapper>();
         final var list = new JBList<>(model);
         methodsWrapped.forEach(model::addElement);
 
+        // Create a popup dialog that displays the list of options
         final var builder = new PopupChooserBuilder<>(list);
 
         builder.setItemChosenCallback(
                 selectedOption -> ApplicationManager.getApplication().invokeLater(
                         () -> ApplicationManager.getApplication().runWriteAction(
-                                () -> {
-                                    final var doc = editor.getDocument();
-                                    CommandProcessor.getInstance().executeCommand(project, () -> insertParameters(editor, selectedOption.method), "Add Auto Parameters", null, DEFAULT, doc);
-                                })));
+                                () -> insertParameterForSelectedOption(project, editor, selectedOption)
+                        )
+                )
+        );
 
         builder.createPopup().showInBestPositionFor(editor);
+    }
+
+    private static void insertParameterForSelectedOption(final Project project, final Editor editor, final PsiMethodWrapper selectedOption) {
+        // Command that will get executed when option is chosen
+        CommandProcessor.getInstance().executeCommand(
+                project,
+                () -> insertParameters(editor, selectedOption.method),
+                "Add Auto Parameters",
+                null,
+                DEFAULT,
+                editor.getDocument()
+        );
     }
 
     private static void insertParameters(final Editor editor, final PsiMethod psiMethod) {
         final PsiParameterList parameterList = psiMethod.getParameterList();
         final PsiParameter[] params = parameterList.getParameters();
-        final var doc = editor.getDocument();
+        final int offset = findCaretOffset(editor, editor.getDocument());
+
+        final var insertString = Arrays.stream(params)
+                .map(PsiParameter::getName)
+                .collect(Collectors.joining(", "));
+
+        editor.getDocument().insertString(offset, insertString);
+        editor.getCaretModel().moveToOffset(offset + insertString.length() + 1);
+    }
+
+    private static int findCaretOffset(final Editor editor, final Document doc) {
         int offset = editor.getCaretModel().getOffset();
         final var textUnderCaret = doc.getText(new TextRange(offset, offset + 1));
         final var textLeftOfCaret = doc.getText(new TextRange(offset - 1, offset));
+
         if (textLeftOfCaret.startsWith("(")) {
-            // do nothing
+            // caret is already at the right spot; do nothing
         } else if (textUnderCaret.startsWith("(")) {
             offset++;
         } else if (!textUnderCaret.startsWith(")")) {
             offset--;
         }
 
-        final var insertString = Arrays.stream(params)
-                .map(PsiParameter::getName)
-                .collect(Collectors.joining(", "));
-        doc.insertString(offset, insertString);
-        editor.getCaretModel().moveToOffset(offset + insertString.length() + 1);
+        return offset;
     }
 
     @Override
@@ -141,40 +192,8 @@ public final class AutoFillCallArguments extends PsiElementBaseIntentionAction i
         }
     }
 
-    private Collection<PsiMethod> resolveMethodFromCandidates(@NotNull final Project project, final Editor editor) {
-        final PsiFile file = PsiUtilBase.getPsiFileInEditor(editor, project);
-
-        final int offset = editor.getCaretModel().getOffset();
-        final int fileLength = file.getTextLength();
-
-        final ShowParameterInfoContext context = new ShowParameterInfoContext(editor, project, file, offset, -1, false, false);
-
-        final int offsetForLangDetection = offset > 0 && offset == fileLength ? offset - 1 : offset;
-        final Language language = PsiUtilCore.getLanguageAtOffset(file, offsetForLangDetection);
-
-        return DumbService.getInstance(project).computeWithAlternativeResolveEnabled(
-                () -> getParameterInfoHandlers(project, file, language)
-                        .stream()
-                        .map(handler -> handler.findElementForParameterInfo(context))
-                        .filter(Objects::nonNull)
-                        .flatMap(element -> Arrays.stream(context.getItemsToShow()))
-                        .map(MethodParameterInfoHandler::tryGetMethodFromCandidate)
-                        .filter(Objects::nonNull)
-                        .filter(PsiMethod::hasParameters)
-                        .toList());
-    }
-
-    @NotNull
-    private static Collection<ParameterInfoHandler> getParameterInfoHandlers(@NotNull final Project project, @NotNull final PsiFile file, @NotNull final Language language) {
-        final var handlers = new HashSet<ParameterInfoHandler>();
-        final var dumbService = DumbService.getInstance(project);
-        for (final var currentLanguage : List.of(language, file.getViewProvider().getBaseLanguage())) {
-            handlers.addAll(dumbService.filterByDumbAwareness(LanguageParameterInfo.INSTANCE.allForLanguage(currentLanguage)));
-        }
-        return handlers;
-    }
-
     private record PsiMethodWrapper(PsiMethod method) {
+
         @Override
         public String toString() {
             final StringJoiner parametersInfo = new StringJoiner(", ");
